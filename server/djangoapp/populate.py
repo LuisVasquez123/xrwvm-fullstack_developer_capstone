68355b8a41f4d407c0bd3080ee2386dc596dbb34
Luis Enrique Vásquez Prado <lvasquezprado@gmail.com>
2025-09-17 11:58:30 +0200

    corrected linting mistakes 3

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -16,7 +16,7 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
-            CarMake.objects.create(name=data["name"], 
+            CarMake.objects.create(name=data["name"],
                                    description=data["description"])
         )
 
@@ -59,10 +59,10 @@ def initiate():
             "car_make": car_make_instances[1],
         },
         {"name": "A4", "type": "SUV", "year": 2023, "car_make":
-          car_make_instances[2]},
-        {"name": "A5", "type": "SUV", "year": 2023, "car_make": 
          car_make_instances[2]},
-        {"name": "A6", "type": "SUV", "year": 2023, "car_make": 
+        {"name": "A5", "type": "SUV", "year": 2023, "car_make":
+         car_make_instances[2]},
+        {"name": "A6", "type": "SUV", "year": 2023, "car_make":
          car_make_instances[2]},
         {
             "name": "Sorrento",
